3a6fc7acbfb74875582471f1d486d51975c3f55b
Cherakala Gireesh <135722119+cherakalagireesh@users.noreply.github.com>
2024-02-27 08:47:04 +0530

    Time: 46 ms (89.61%), Space: 48.9 MB (31.37%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    
+    let left = 0;
+    let right = 0;
+
+    while(right < nums.length)
+    {
+        if(nums[right] !== val)
+        {
+            nums[left] = nums[right];
+            left++;
+        }
+
+        right++;
+    }
+
+    return left;
+};
